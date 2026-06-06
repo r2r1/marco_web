a0af507c7334cba1dafa69445889252553860d03
r2r1 <grishaivan8@gmail.com>
2026-06-06 14:41:14 +0300

    ыф

--- marco-site/lib/site-contact.ts
+++ marco-site/lib/site-contact.ts
@@ -2,7 +2,6 @@ export const CONTACT_PHONE = '+79331797333';
 export const CONTACT_PHONE_DISPLAY = '+7 (933) 179-73-33';
 export const CONTACT_EMAIL = 'marco-kmv@yandex.ru';
 
-
 export const LEAD_FORM_SUCCESS_MESSAGE =
   'Спасибо. Свяжемся с вами в течение 15 минут.';
 
